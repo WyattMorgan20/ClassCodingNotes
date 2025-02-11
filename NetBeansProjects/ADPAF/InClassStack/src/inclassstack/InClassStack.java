64eb0e74dadd1b8aa0d8750c07c2a805999b2b8a
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-11 08:31:17 -0600

    Started stack notes

---
+++ NetBeansProjects/ADPAF/InClassStack/src/inclassstack/InClassStack.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inclassstack;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author s540549
+ */
+public class InClassStack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Stack<String> stkS = new Stack<>();
+        Stack<Integer> stkI = new Stack<>();
+        
+        System.out.println("Size of stkS: " + stkS.size());
+        System.out.println("Size of stkI: " + stkI.size());
+        
+    }
+    
+}
