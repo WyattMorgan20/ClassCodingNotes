e7116957543dbfe0da3c8392e9143574f5017159
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-11 08:42:04 -0600

    finished stack notes

--- NetBeansProjects/ADPAF/InClassStack/src/inclassstack/InClassStack.java
+++ NetBeansProjects/ADPAF/InClassStack/src/inclassstack/InClassStack.java
@@ -23,6 +23,25 @@ public class InClassStack {
         System.out.println("Size of stkS: " + stkS.size());
         System.out.println("Size of stkI: " + stkI.size());
         
+        stkS.push("Wyatt");
+        stkS.push("Morgan");
+        stkS.push("Max");
+        stkS.push("DeJesus");
+        
+        System.out.println("Size of stkS: " + stkS.size());
+        
+        System.out.println("Pop: " + stkS.pop()); // removes and returns top element in stack
+        System.out.println("Peek: " + stkS.peek()); // returns top element in stack WITHOUT removing
+        
+        System.out.println("Stack elements:");
+        int size = stkS.size();
+        for(int i = 0; i < size; i++){
+            System.out.print(stkS.pop() + " ");
+        }
+        
+        System.out.println("\nIs Empty: " + stkS.isEmpty());
+        
+        
     }
     
 }
