a1c0918e6e18875e8078993102a0349e8e700d41
Wyatt Morgan <iamtheyy@gmail.com>
2025-02-25 09:03:27 -0600

    finished arrayQueues code for the day

--- NetBeansProjects/ADPAF/ArrayQueues/src/arrayqueues/ArrayQueues.java
+++ NetBeansProjects/ADPAF/ArrayQueues/src/arrayqueues/ArrayQueues.java
@@ -20,7 +20,7 @@ public class ArrayQueues {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        ArrayQueues arr = new ArrayQueues(5);
     }
     
     public ArrayQueues(int capacity){
