ecbaeae90072bc72dbbbfd2620196fc726048335
Wyatt Morgan <iamtheyy@gmail.com>
2025-03-20 08:43:52 -0500

    finished code up to inclassactivity

--- NetBeansProjects/ADPAF/InClassHashMaps/src/inclasshashmaps/InClassHashMaps.java
+++ NetBeansProjects/ADPAF/InClassHashMaps/src/inclasshashmaps/InClassHashMaps.java
@@ -4,6 +4,8 @@
  */
 package inclasshashmaps;
 
+import java.util.*;
+
 /**
  *
  * @author s540549
@@ -14,7 +16,31 @@ public class InClassHashMaps {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Map<Integer, String> map1 = new HashMap<>();
         
+        // Student id and names:
+        map1.put(91987, "Wyatt");
+        map1.put(91964, "Max");
+        map1.put(91952, "Jen");
+        map1.put(91912, "Meg");
+        
+        System.out.println("Maps " + map1);
+        
+        // Printing hashcode for every pair using entrySet() method:
+        for(Map.Entry<Integer, String> entry: map1.entrySet()){
+            System.out.println("Hashcode for keyvalue pair " + entry.hashCode());
+        }
+        
+        // printing keys using keySet() method:
+        Set<Integer> keys = map1.keySet();
+        
+        System.out.println("Keys in the map ");
+        for(Integer k: keys){
+            System.out.println(k);
+        }
+        
+        // Printing all values using values() method:
+        System.out.println("Values in the map " + map1.values());
     }
     
 }
