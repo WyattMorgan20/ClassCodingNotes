5ac2bf64b852b7885198563ea68a2ef8ee1ecfda
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-18 19:54:09 -0500

    half way through video

--- Eclipse/ParametersDemo/src/main/java/com/nwmsu/ParametersDemo/Controller/DogController.java
+++ Eclipse/ParametersDemo/src/main/java/com/nwmsu/ParametersDemo/Controller/DogController.java
@@ -1,5 +1,29 @@
 package com.nwmsu.ParametersDemo.Controller;
 
-public class DogController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class DogController {
+	
+//	@RequestMapping("display")
+//	public String display(HttpServletRequest request) {
+//		HttpSession session = request.getSession();
+//		String dognameIn = request.getParameter("dogName");
+//		session.setAttribute("dogNameDisplay", dognameIn);
+//		return "display.html";
+//	}
+	
+//	@RequestMapping("display")
+//	public String display(@RequestParam("maskedName") String dogName, HttpSession session) {
+//		session.setAttribute("dogNameDisplay", dogName);
+//		return "display.html";
+//	}
+	
+	
+	
 }
