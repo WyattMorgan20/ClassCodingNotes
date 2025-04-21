bc022e9490f1002bf6e08504d73553e20ccbf97f
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-21 17:56:51 -0500

    finished Project Milestone 3

--- Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Controllers/DogController.java
+++ Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Controllers/DogController.java
@@ -1,14 +1,58 @@
 package com.nwmsu.DMS.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nwmsu.DMS.Models.Dog;
+import com.nwmsu.DMS.repository.DogRepository;
 
 @Controller
 public class DogController {
+	
+	ModelAndView mv = new ModelAndView();
+	
+	@Autowired
+	DogRepository dogRepo;
 
+//	@RequestMapping("dogHome")
+//	public String home() {
+//		return "home";
+//	}
+	
 	@RequestMapping("dogHome")
-	public String home() {
-		return "home";
+	public ModelAndView home() {
+		mv.setViewName("home");
+		return mv;
+	}
+	
+	@RequestMapping("add")
+	public ModelAndView add() {
+		mv.setViewName("addNewDog.html");
+		return mv;
+	}
+	
+	@RequestMapping("addNewDog")
+	public ModelAndView addNewDog(Dog dog) {
+		dogRepo.save(dog);
+		mv.setViewName("home");
+		return mv;
+	}
+	
+	@RequestMapping("viewModifyDelete")
+	public ModelAndView viewDogs() {
+		mv.setViewName("viewDogs");
+		Iterable<Dog> dogList = dogRepo.findAll();
+		mv.addObject("dogs", dogList);
+		return mv;
+	}
+	
+	@RequestMapping("editDog")
+	public ModelAndView editDog(Dog dog) {
+		dogRepo.save(dog);
+		mv.setViewName("editDog");
+		return mv;
 	}
 	
 }
