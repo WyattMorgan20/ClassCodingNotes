d890ba5979ef86dfc2966f7e51c8d198bc2d0e5a
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-21 16:56:52 -0500

    finished video 2 milestone 3

--- Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Controllers/DogController.java
+++ Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Controllers/DogController.java
@@ -1,7 +1,14 @@
 package com.nwmsu.DMS.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class DogController {
 
-	
+	@RequestMapping("dogHome")
+	public String home() {
+		return "home";
+	}
 	
 }
--- Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/repository/DogRepository.java
+++ Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/repository/DogRepository.java
@@ -2,7 +2,9 @@ package com.nwmsu.DMS.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface DogRepository extends CrudRepository {
+import com.nwmsu.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer> {
 
 	
 	
