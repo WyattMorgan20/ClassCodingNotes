63e8622a2f03554569cc9a3c76687a491731309d
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-22 14:42:59 -0500

    finished milestone 4

--- Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Models/Dog.java
+++ Eclipse/DogsManagementSystem/src/main/java/com/nwmsu/DMS/Models/Dog.java
@@ -2,13 +2,18 @@ package com.nwmsu.DMS.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Dog {
+	
 	@Id
 	private int id;
 	private String name;
 	private String breed;
+	
+	@ManyToOne
+	private Trainer trainer;
 	
 	public int getId() {
 		return id;
@@ -34,11 +39,17 @@ public class Dog {
 		this.breed = breed;
 	}
 	
-	@Override
-	public String toString() {
-		return "Dog [id=" + id + ", name=" + name + ", breed=" + breed + "]";
+	public Trainer getTrainer() {
+		return trainer;
 	}
 	
+	public void setTrainer(Trainer trainer) {
+		this.trainer = trainer;
+	}
 	
+	@Override
+	public String toString() {
+		return "Dog [id=" + id + ", name=" + name + ", breed=" + breed + ", trainer=" + trainer + "]";
+	}
 	
 }
